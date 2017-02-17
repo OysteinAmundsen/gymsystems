ba75e255dfc889604c3af37b40b9bb83c4ae0be4
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-02-17 13:05:32 +0100

    upgraded angular-cli

--- upgrade-cli.cmd
+++ upgrade-cli.cmd
@@ -1,4 +1,5 @@
 rm -rf node_modules dist tmp
-cmd /C npm i angular-cli@latest -D
+cmd /C npm i @angular/cli@latest -D
 cmd /C npm install
-ng init --source-dir=client --style=scss --routing=true
+
+rem ng init --source-dir=client --style=scss --routing=true
