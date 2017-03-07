8b95eedbe9c0fc1ee9c8b461d8a81155227595d6
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-03-07 14:28:21 +0100

    Bugfix on floating labels

--- client/app/shared/floating-label/floating-label-container.directive.ts
+++ client/app/shared/floating-label/floating-label-container.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, ElementRef, Renderer, OnInit, AfterViewChecked } from '@angular/core';
+import { AfterViewChecked, Directive, ElementRef, OnInit, Renderer, ViewChild } from '@angular/core';
 
 @Directive({
   selector: '[appFloatingLabelContainer]'
 })
 export class FloatingLabelContainerDirective implements OnInit, AfterViewChecked {
   inputElm: HTMLInputElement;
+  labelElm: HTMLLabelElement;
   constructor(private host: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
     this.inputElm = this.host.nativeElement.querySelector('input,textarea,select');
+    this.labelElm = this.host.nativeElement.querySelector('label');
     if (!this.inputElm) {
       throw new Error('No input element found in this floating label container');
     }
@@ -18,6 +20,7 @@ export class FloatingLabelContainerDirective implements OnInit, AfterViewChecked
     this.renderer.listen(this.inputElm, 'input', event => this.onValueChanged());
     this.renderer.listen(this.inputElm, 'focus', event => this.onFocusChanged());
     this.renderer.listen(this.inputElm, 'blur', event => this.onFocusChanged());
+    this.renderer.listen(this.labelElm, 'click', event => this.onLabelClick());
   }
 
   ngAfterViewChecked() {
@@ -31,5 +34,9 @@ export class FloatingLabelContainerDirective implements OnInit, AfterViewChecked
   onFocusChanged() {
     this.renderer.setElementClass(this.host.nativeElement, 'hasFocus', document.activeElement === this.inputElm);
     this.onValueChanged();
+  }
+
+  onLabelClick() {
+    this.inputElm.focus();
   }
 }
