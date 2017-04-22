37f41c80edd6ae5a560427b7e5ed1dea50947760
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-04-22 21:47:57 +0200

    Trying to make something of the home page

--- client/app/views/home/home.component.ts
+++ client/app/views/home/home.component.ts
@@ -11,12 +11,13 @@ import { ITournament } from 'app/services/model/ITournament';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   types = [];
+  current = [];
   isLoading: boolean = true;
 
   constructor(private tournamentService: TournamentService) {
-    tournamentService.past().subscribe(tournaments => this.types.push({ name: 'Past', tournaments: tournaments }));
-    tournamentService.current().subscribe(tournaments => this.types.push({ name: 'Current', tournaments: tournaments }));
     tournamentService.upcoming().subscribe(tournaments => this.types.push({ name: 'Future', tournaments: tournaments }));
+    tournamentService.past().subscribe(tournaments => this.types.push({ name: 'Past', tournaments: tournaments }));
+    tournamentService.current().subscribe(tournaments => this.current = tournaments);
   }
 
   ngOnInit() { }
