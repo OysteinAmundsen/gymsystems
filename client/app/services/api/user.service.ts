bd01366196d368b6a5519dddd6f8cf9d15a7bc91
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-04-25 14:00:53 +0200

    Recheck auth every 10 minutes
    Disable auth checking after first check, reenable if user logs in.

--- client/app/services/api/user.service.ts
+++ client/app/services/api/user.service.ts
@@ -17,40 +17,53 @@ export class UserService {
   _isLoadingMe: boolean = false;
   _meObservable = new ReplaySubject(1);
   _currentUser: IUser;
+  _recheck;
   private get currentUser(): IUser { return this._currentUser; }
   private set currentUser(user: IUser) {
-    this._currentUser = user;
-    this._meObservable.next(user);
+    if (this._currentUser !== user) {
+      this._currentUser = user;
+      this._meObservable.next(user);
+    }
+    if (this._recheck) { clearTimeout(this._recheck); }
+    if (user) {
+      // Set to recheck auth after 10 minutes. Will return null if users session has expired.
+      this._recheck = setTimeout(() => this._loadMeInternal().subscribe(), 10 * 60 * 1000);
+    }
   }
 
   constructor(private http: Http) {  }
 
-  all(): Observable<IUser[]> {
-    return this.http.get('/api/users').map((res: Response) => res.json()).share();
-  }
-
-  getById(id: number): Observable<IUser> {
-    return this.http.get('/api/users/get/' + id).map((res: Response) => res.json()).share();
-  }
-
+  // AUTH functions
   private userReceived(res: Response) {
     this._isLoadingMe = false;
     this.currentUser = res instanceof Response ? res.json() : res;
     return this.currentUser;
   }
 
+  private _loadMeInternal() {
+    return this.http.get('/api/users/me')
+      .map((res: Response) => this.userReceived(res))
+      .catch((err: Response) =>  {
+        this.userReceived(null);
+        return Observable.throw(err);
+      });
+  }
+
   getMe(): Observable<IUser> {
-    if (!this.currentUser && !this._isLoadingMe) {
+    if (this.currentUser === undefined && !this._isLoadingMe) {
       this._isLoadingMe = true; // Prevent loading if load allready initiated
-      this.http.get('/api/users/me')
-        .map((res: Response) => this.userReceived(res))
-        .catch((err: Response) =>  {
-          this.userReceived(null);
-          return Observable.throw(err);
-        })
-        .subscribe();
+      this._loadMeInternal().subscribe();
     }
     return this._meObservable;
+  }
+
+  // Standard REST api functions
+  all(): Observable<IUser[]> {
+    return this.http.get('/api/users').map((res: Response) => res.json()).share();
+  }
+
+  getById(id: number): Observable<IUser> {
+    return this.http.get('/api/users/get/' + id).map((res: Response) => res.json()).share();
   }
 
   save(user: IUser): Observable<IUser> {
