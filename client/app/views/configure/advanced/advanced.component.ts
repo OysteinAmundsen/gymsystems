a2dd511df8a6132b016c7979e580e36ac0ec50df
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-04-27 10:47:30 +0200

    Added possibility to change default values in Advanced settings
    This is a really crude implementation. User must be technical, and have an understanding of how JSON works.

--- client/app/views/configure/advanced/advanced.component.ts
+++ client/app/views/configure/advanced/advanced.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { ConfigurationService } from "app/services/api";
 
 @Component({
   selector: 'app-advanced',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced.component.scss']
 })
 export class AdvancedComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChild('area') textAreas: ElementRef;
+  configuration;
+  selected;
+  get defaultValues() {
+    return this.configuration ? this.configuration.find(c => c.name === 'defaultValues').value : null;
+  }
+  get defaultValueKeys() {
+    return this.configuration ? Object.keys(this.defaultValues) : null;
   }
 
+  constructor(private config: ConfigurationService) { }
+
+  ngOnInit() {
+    this.config.all().subscribe(res => {
+      this.configuration = res;
+      this.selected = this.defaultValueKeys[0];
+    });
+  }
+
+  getRows(v) {
+    if (Array.isArray(v)) { return 2 + (Object.keys(v[0]).length + 2) * v.length; }
+    return 2 + Object.keys(v).length;
+  }
+
+  valueChanged(key) {
+    this.defaultValues[key] = JSON.parse(this.textAreas.nativeElement.value);
+  }
+
+  save() {
+    this.config.save(this.configuration).subscribe(res => this.configuration = res);
+  }
 }
