680313f673340adc48e18cdeb6bc793f2c94d33f
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-09-04 16:05:16 +0200

    Fixed tests

--- client/app/views/event/signoff-report/signoff-report.component.spec.ts
+++ client/app/views/event/signoff-report/signoff-report.component.spec.ts
@@ -1,6 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Router, ActivatedRoute } from '@angular/router/src';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'app/app.module';
+
+import { SharedModule } from 'app/shared/shared.module';
+
 import { SignoffReportComponent } from './signoff-report.component';
+import { EventComponent } from 'app/views/event/event.component';
+
+import { ScheduleService, TeamsService, ScoreService } from 'app/services/api';
+
+import { DummyParent } from 'app/views/event/list/list.component.spec';
+import { ScheduleServiceStub } from 'app/services/api/schedule/schedule.service.stub';
+import { TeamsServiceStub } from 'app/services/api/teams/teams.service.stub';
+import { ScoreServiceStub } from 'app/services/api/score/score.service.stub';
 
 describe('SignoffReportComponent', () => {
   let component: SignoffReportComponent;
@@ -8,7 +26,26 @@ describe('SignoffReportComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignoffReportComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpModule,
+        SharedModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+      ],
+      declarations: [ SignoffReportComponent ],
+      providers: [
+        {provide: EventComponent, useClass: DummyParent},
+        {provide: ScheduleService, useClass: ScheduleServiceStub},
+        {provide: TeamsService, useClass: TeamsServiceStub},
+        {provide: ScoreService, useClass: ScoreServiceStub},
+      ]
     })
     .compileComponents();
   }));
