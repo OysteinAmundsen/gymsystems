626be500a87756aac66c7c2ee54d957db7440671
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-09-07 15:44:35 +0200

    Optimization on UpperFormControl

--- client/app/shared/form/UppercaseFormControl.ts
+++ client/app/shared/form/UppercaseFormControl.ts
@@ -1,10 +1,19 @@
 import { FormControl, ValidatorFn, AsyncValidatorFn } from '@angular/forms';
 
 export class UppercaseFormControl extends FormControl {
-  constructor(formState?: any, validator?: ValidatorFn | ValidatorFn[] | null, asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null) {
+  constructor(
+    formState?: any,
+    validator?: ValidatorFn | ValidatorFn[] | null,
+    asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null
+  ) {
     super(formState, validator, asyncValidator);
     this.valueChanges.distinctUntilChanged().subscribe((t: string) => {
-      this.setValue(t.toUpperCase());
+      this.setValue(t.toUpperCase(), {
+        onlySelf: true,
+        emitEvent: false,
+        emitModelToViewChange: true,
+        emitViewToModelChange: false
+      });
     });
   }
 }
