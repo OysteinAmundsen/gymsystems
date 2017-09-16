eefaedd13bee6266c83a62050a9431ff53f45fe1
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-09-16 22:51:51 +0200

    (#156) Tests

--- client/app/views/configure/venue/venue-editor/venue-editor.component.spec.ts
+++ client/app/views/configure/venue/venue-editor/venue-editor.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppModuleTest } from 'app/app.module.spec';
 import { VenueEditorComponent } from './venue-editor.component';
+import { AgmCoreModule } from '@agm/core';
+import { VenueService } from 'app/services/api';
 
 describe('VenueEditorComponent', () => {
   let component: VenueEditorComponent;
@@ -10,7 +12,10 @@ describe('VenueEditorComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        AppModuleTest
+        AppModuleTest,
+        AgmCoreModule.forRoot({
+          apiKey: VenueService.apiKey
+        })
       ],
       declarations: [ VenueEditorComponent ]
     }).compileComponents();
