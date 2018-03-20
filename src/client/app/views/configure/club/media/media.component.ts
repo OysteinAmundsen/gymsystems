49d99c2f94a435dedf6ab48983c37e63658355d6
Øystein Amundsen <oystein.amundsen@bouvet.no>
2018-03-20 22:06:48 +0100

    Fixed erroneous gitignore entry

---
+++ src/client/app/views/configure/club/media/media.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media',
+  templateUrl: './media.component.html',
+  styleUrls: ['./media.component.scss']
+})
+export class MediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
